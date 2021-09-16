4977c0c37a2d6668cf842427f103f0b7d3a14ad7
koebe1 <koe.be.pay@web.de>
2021-09-16 10:02:27 +0200

    added card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import Nav from "./components/nav";
+import studioMkPlay from "./ressources/images/studioMkPlay.png";
 import "./App.css";
 
 function Grain(props) {
   return <div className="grain-container">{props.children}</div>;
+}
+
+function Card(props) {
+  return (
+    <div
+      className="card-container"
+      style={{ width: props.width, height: props.height }}
+    >
+      {props.children}
+    </div>
+  );
 }
 
 function App() {
@@ -10,6 +22,11 @@ function App() {
     <div className="App">
       <Grain>
         <Nav />
+        <section className="section-1">
+          <Card width="60vw" height="80vh">
+            <img src={studioMkPlay} alt="studioMK website play page" />
+          </Card>
+        </section>
       </Grain>
     </div>
   );
