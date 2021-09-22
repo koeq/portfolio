d179d7ae6e68b9a37009e3af2be53d4909cef4ad
koebe1 <koe.be.pay@web.de>
2021-09-22 21:05:17 +0200

    added prototype landing page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
-import Nav from "./components/nav";
-import Grain from "./components/grain";
 // import studioMkPlay from "./ressources/images/studioMkPlay.png";
 import "./App.css";
+import Grain from "./components/grain";
+import Nav from "./components/nav";
 
 // function Card(props) {
 //   return (
@@ -15,19 +15,59 @@ import "./App.css";
 //   );
 // }
 
+/* <Card width="60vw" height="80vh">
+            <img src={studioMkPlay} alt="studioMK website play page" />
+          </Card> */
+
+function Landing() {
+  return (
+    <section className="section-1">
+      <div className="grid-container">
+        <span>F</span>
+        <span>R</span>
+        <span>O</span>
+        <span>N</span>
+        <span>T</span>
+        <span>E</span>
+        <span className="symbol">·</span>
+        <span>N</span>
+        <span className="symbol">·</span>
+        <span>D</span>
+        <span>E</span>
+        <span>N</span>
+        <span>G</span>
+        <span className="symbol">·</span>
+        <span className="symbol">·</span>
+        <span>I</span>
+        <span>N</span>
+        <div className="grid-container-text">
+          <span>Benedikt König</span>
+          <p>
+            I’m a freelance award winning designer and developer in the UK. With
+            over 20 years of experience and self-learning, my goal is to
+            innovate in digital and help brands that want to do that too. I work
+            at the crossroads of digital product design and digital development.
+            Having the ability to connect both visual and function seemlessly
+            has proven invaluable to all I’ve worked with.
+          </p>
+        </div>
+
+        <span>E</span>
+        <span>E</span>
+        <span>R</span>
+        <span className="symbol">·</span>
+        <span className="symbol">·</span>
+      </div>
+    </section>
+  );
+}
+
 function App() {
   return (
     <div className="App light-theme">
       <Grain>
         <Nav />
-        <section className="section-1">
-          <div className="heading">
-            <span>hello</span>
-          </div>
-          {/* <Card width="60vw" height="80vh">
-            <img src={studioMkPlay} alt="studioMK website play page" />
-          </Card> */}
-        </section>
+        <Landing />
       </Grain>
     </div>
   );
--- src/components/nav.js
+++ src/components/nav.js
@@ -11,11 +11,10 @@ export default function Nav() {
   // deactivate scrolling in nav-slide-menu
   useEffect(() => {
     const html = document.getElementsByTagName("html")[0];
-    {
-      isActive
-        ? (html.style.overflow = "hidden")
-        : (html.style.overflow = "scroll");
-    }
+
+    isActive
+      ? (html.style.overflow = "hidden")
+      : (html.style.overflow = "scroll");
   }, [isActive]);
 
   const navElements = [
@@ -23,89 +22,94 @@ export default function Nav() {
       toggled={isActive}
       toggle={setActive}
       onToggle={handleToggle}
+      size={32}
       direction="right"
       distance="s,"
       duration={0.4}
+      color="#fff"
       easing="ease"
     />,
-    "œ",
-    "œ",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
     "œ"
   ];
 
   return (
-    <React.Fragment>
-      <div className="nav-container">
-        <ul className={isActive ? "nav-list nav-list-active" : "nav-list"}>
-          {navElements.map((element, index) => {
-            return (
-              <li key={index} className="nav-element">
-                {element}
-              </li>
-            );
-          })}
-        </ul>
-        <hr className="nav-border-dark" />
+    <div className="nav-container">
+      <ul className={isActive ? "nav-list nav-list-active" : "nav-list"}>
+        {navElements.map((element, index) => {
+          return (
+            <li key={index} className="nav-element">
+              {element}
+            </li>
+          );
+        })}
+      </ul>
+      <hr className="nav-border-dark" />
 
+      <div className={isActive ? "slide-menu-active slide-menu" : "slide-menu"}>
         <div
-          className={isActive ? "slide-menu-active slide-menu" : "slide-menu"}
+          className={
+            isActive
+              ? "slide-menu-item slide-menu-item-active"
+              : "slide-menu-item"
+          }
         >
-          <div
+          About
+          <span
             className={
               isActive
-                ? "slide-menu-item slide-menu-item-active"
-                : "slide-menu-item"
+                ? "slide-menu-item-sub1 slide-menu-item-sub-active"
+                : "slide-menu-item-sub1"
             }
           >
-            About
-            <span
-              className={
-                isActive
-                  ? "slide-menu-item-sub1 slide-menu-item-sub-active"
-                  : "slide-menu-item-sub1"
-              }
-            >
-              Benedikt König
-            </span>
-          </div>
-          <div
+            Benedikt König
+          </span>
+        </div>
+        <div
+          className={
+            isActive
+              ? "slide-menu-item slide-menu-item-active"
+              : "slide-menu-item"
+          }
+        >
+          Projects
+          <span
             className={
               isActive
-                ? "slide-menu-item slide-menu-item-active"
-                : "slide-menu-item"
+                ? "slide-menu-item-sub2 slide-menu-item-sub-active"
+                : "slide-menu-item-sub2"
             }
           >
-            Projects
-            <span
-              className={
-                isActive
-                  ? "slide-menu-item-sub2 slide-menu-item-sub-active"
-                  : "slide-menu-item-sub2"
-              }
-            >
-              Have a look
-            </span>
-          </div>
-          <div
+            Have a look
+          </span>
+        </div>
+        <div
+          className={
+            isActive
+              ? "slide-menu-item slide-menu-item-active"
+              : "slide-menu-item"
+          }
+        >
+          Contact
+          <span
             className={
               isActive
-                ? "slide-menu-item slide-menu-item-active"
-                : "slide-menu-item"
+                ? "slide-menu-item-sub3 slide-menu-item-sub-active"
+                : "slide-menu-item-sub3"
             }
           >
-            Contact
-            <span
-              className={
-                isActive
-                  ? "slide-menu-item-sub3 slide-menu-item-sub-active"
-                  : "slide-menu-item-sub3"
-              }
-            >
-              Send me a message
-            </span>
-          </div>
+            Send me a message
+          </span>
         </div>
       </div>
-    </React.Fragment>
+    </div>
   );
 }
