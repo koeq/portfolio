a46736de725b6becb705f5791c696e1c40c18814
koebe1 <koe.be.pay@web.de>
2021-09-24 16:28:31 +0200

    finished first landing animattion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./App.css";
 import Grain from "./components/grain";
 import Nav from "./components/nav";
@@ -78,33 +78,85 @@ function Landing() {
   const contentListOne = ["F", "R", "O", "N", "T"];
   const contentListTwo = ["E", "·", "N", "·", "D"];
 
-  const { scrollY } = useViewportScroll();
+  const [animationOffset, setAnimationOffset] = useState(0);
+
+  const [yProgress, setYProgress] = useState(0);
+  const [textRowWidth, setTextRowWidth] = useState(0);
+  const [vw, setVw] = useState(0);
+
+  const textRowRef = useRef(null);
+
+  // progress of scroll in landing container
+  const { scrollYProgress } = useViewportScroll();
+
+  // change animation offset on window resize
+  const handleResize = () => {
+    setTextRowWidth(textRowRef.current.offsetWidth);
+    // get vw cross-browser
+    setVw(
+      Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+      )
+    );
+  };
+
+  useEffect(() => {
+    // width of fronted text container
+    setTextRowWidth(textRowRef.current.offsetWidth);
+    // get vw cross-browser
+    setVw(
+      Math.max(
+        document.documentElement.clientWidth || 0,
+        window.innerWidth || 0
+      )
+    );
+    //
+    const scrollListener = window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("scroll", scrollListener);
+    };
+  }, []);
+
+  useEffect(() => {
+    // get distance text has to be approx. (bc of transform scale)
+    // moved out of the screen in pixels
+
+    setAnimationOffset(textRowWidth * 2 + (vw - textRowWidth));
+    // move text according to scroll postion in landing container
+  }, [vw, textRowWidth]);
+
   const xPosAnimOne = useTransform(
-    scrollY,
-    [0, 100, 200, 300, 400, 500, 600],
-    [0, -200, -500, -1000, -1500, -2000, -2000]
+    scrollYProgress,
+    [0, -1],
+    [0, -animationOffset]
   );
   const xPosAnimTwo = useTransform(
-    scrollY,
-    [0, 100, 200, 300, 400, 500, 600],
-    [0, 200, 500, 1000, 1500, 2000, 2000]
+    scrollYProgress,
+    [0, -1],
+    [0, animationOffset]
   );
 
   return (
     <div className="landing-container">
       <motion.div
         className="flex-center frontend-container"
-        animate={{ opacity: 1, translateY: -30, scale: 0.95 }}
-        transition={{ duration: 2, delay: 0.5, ease: "easeInOut" }}
+        animate={{ opacity: 1, translateY: -30, scale: 0.9 }}
+        transition={{ duration: 1.5, delay: 0.5, ease: "easeInOut" }}
       >
-        <motion.div style={{ x: xPosAnimOne }} className="frontend-row">
+        <motion.div
+          ref={textRowRef}
+          style={{ x: xPosAnimOne }}
+          className="text-row"
+        >
           {contentListOne.map((content, index) => (
             <span key={index} className="landing-text">
               {content}
             </span>
           ))}
         </motion.div>
-        <motion.div style={{ x: xPosAnimTwo }} className="frontend-row">
+        <motion.div style={{ x: xPosAnimTwo }} className="text-row">
           {contentListTwo.map((content, index) => (
             <span key={index} className="landing-text">
               {content}
@@ -135,7 +187,5 @@ window.onscroll = function() {
     left: 0
   });
 };
-
-
 
 export default App;
